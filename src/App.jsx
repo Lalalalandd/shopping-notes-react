9c4058cb2c96c369c0e497ff03ea0fc2d3b377af
Jefri Achmad Maulana <jefriachmadm@gmail.com>
2024-09-09 09:45:09 +0700

    can add shopping notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const groceryItems = [
   {
@@ -21,11 +22,17 @@ const groceryItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState(groceryItems);
+
+  function handleAddItem(item) {
+    setItems([...items, item]);
+  }
+
   return (
     <div className="app">
       <Header />
-      <Form />
-      <GroceryList />
+      <Form onAddItem={handleAddItem} />
+      <GroceryList items={items} />
       <Footer />
     </div>
   );
@@ -35,31 +42,55 @@ function Header() {
   return <h1>Catatan Belanjaku 📝</h1>;
 }
 
-function Form() {
+function Form({ onAddItem }) {
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name) return;
+
+    const newItem = { name, quantity, checked: false, id: Date.now() };
+    onAddItem(newItem);
+    console.log(newItem);
+    setName("");
+    setQuantity(1);
+  }
+
+  const quantityNum = [...Array(20)].map((_, i) => (
+    <option value={i + 1} key={i + 1}>
+      {i + 1}
+    </option>
+  ));
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Hari ini belanja apa kita?</h3>
       <div>
-        <select>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
+        <select
+          value={quantity}
+          onChange={(e) => setQuantity(Number(e.target.value))}
+        >
+          {quantityNum}
         </select>
-        <input type="text" placeholder="nama barang..." />
+        <input
+          type="text"
+          placeholder="nama barang..."
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
       <button>Tambah</button>
     </form>
   );
 }
 
-function GroceryList() {
+function GroceryList({ items }) {
   return (
     <>
       <div className="list">
         <ul>
-          {groceryItems.map((item) => (
+          {items.map((item) => (
             <Item item={item} key={item.id} />
           ))}
         </ul>
@@ -76,7 +107,7 @@ function GroceryList() {
   );
 }
 
-function Item({item}) {
+function Item({ item }) {
   return (
     <li key={item.id}>
       <input type="checkbox" checked={item.checked} />
